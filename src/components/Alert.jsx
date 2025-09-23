2c209da2aa86e5feab649736d8b2f39eed68c88f
Daniel <programador2@intareactiva.net.co>
2025-09-23 11:26:27 -0500

    Alert css adjustmen

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -3,7 +3,7 @@ import { IoCloseOutline } from "react-icons/io5";
 
 function Alert() {
   return (
-    <div className="relative shadow-lg rounded-2xl p-2 max-w-96 alert">
+    <div className="relative shadow-lg rounded-2xl p-2 max-w-96">
       {/* El botón "X" se posiciona de forma absoluta DENTRO del cuadro */}
       <div className="absolute -top-3 -right-3 w-12 h-12 rounded-full bg-white flex items-center justify-center">
         <div className="w-10 h-10 rounded-full bg-primary flex items-center justify-center text-white text-xl">
@@ -12,11 +12,11 @@ function Alert() {
       </div>
 
       {/* El resto de tu contenido sigue igual */}
-      <div className="alert-body">
-        <div className="border-b-3 border-primary px-5 py-2 text-primary font-bold text-3xl">
+      <div className="alert">
+        <div className="alert-header">
           ¡Ups!
         </div>
-        <div className="p-3 font-semibold text-lg text-[#414141]">
+        <div className="alert-body">
           Ocurrió un error, por favor digita <br /> de nuevo tu código de
           reserva.
         </div>
