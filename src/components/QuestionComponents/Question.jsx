d33484082d72670d8df8ceaa19f7dc37487c147a
Daniel <programador2@intareactiva.net.co>
2025-09-23 14:44:13 -0500

    Add Question and Option components; update QuestionView to render them with options

---
+++ src/components/QuestionComponents/Question.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Question() {
+  return (
+    <div className="question bg-gradient-to-l from-primary to-secondary w-full max-w-5/6  p-1 rounded-full my-5">
+      <div className="w-full bg-white rounded-full px-20 py-1">
+        <p className="text-primary text-center text-2xl">
+          ¿Cuál es el único equipo campeón colombiano de la Copa Sudamericana?
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Question;
--- src/views/QuestionView.jsx
+++ src/views/QuestionView.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import Question from "../components/QuestionComponents/Question";
+import Option from "../components/QuestionComponents/Option";
+
+// 1. Define tus opciones en un array de datos
+const optionsData = [
+  { letter: "A", text: "Deportivo Cali" },
+  { letter: "B", text: "América de Cali" },
+  { letter: "C", text: "Millonarios FC" },
+  { letter: "D", text: "Atlético Nacional" },
+];
 
 function QuestionView() {
   return (
-    <div>QuestionView</div>
-  )
+    <div>
+      <div>Timer</div>
+      <div>Elige la respuesta correcta:</div>
+      <div className="flex flex-col justify-center items-center w-full max-w-1/2">
+        <div>
+          <Question />
+        </div>
+
+        <div className="grid grid-cols-2 gap-5">
+          {optionsData.map((option) => (
+            <Option
+              key={option.letter}
+              text={option.text}
+              letter={option.letter}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div>Logos avianca</div>
+    </div>
+  );
 }
 
-export default QuestionView
+export default QuestionView;
