96d45caf199c95ce78be160cf94b56e41485e2b8
Daniel <programador2@intareactiva.net.co>
2025-09-23 15:08:28 -0500

    nice

--- src/views/QuestionView.jsx
+++ src/views/QuestionView.jsx
@@ -12,22 +12,24 @@ const optionsData = [
 
 function QuestionView() {
   return (
-    <div>
+    <div className="w-full flex flex-col justify-center items-center">
       <div>Timer</div>
       <div>Elige la respuesta correcta:</div>
-      <div className="flex flex-col justify-center items-center w-full max-w-1/2">
-        <div>
+      <div className="w-full flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center w-full max-w-1/2">
           <Question />
-        </div>
 
-        <div className="grid grid-cols-2 gap-5">
-          {optionsData.map((option) => (
-            <Option
-              key={option.letter}
-              text={option.text}
-              letter={option.letter}
-            />
-          ))}
+          <div className="grid grid-cols-2 gap-5 w-full">
+            {optionsData.map((option) => (
+              <div className="flex justify-center items-center">
+                <Option
+                  key={option.letter}
+                  text={option.text}
+                  letter={option.letter}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 
