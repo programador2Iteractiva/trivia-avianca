37ac889bc41171dd38ea04d7a2d26a6934f817ab
Daniel <programador2@intareactiva.net.co>
2025-09-29 15:05:22 -0500

    Cambio en el Home

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -2,6 +2,11 @@ import BtnLayout from "../components/utils/BtnLayout";
 import Footer from "../components/utils/Footer";
 import LogosAvianca from "../components/LogosAvianca";
 import LogoBienvenidos from "../assets/LogoBienvenidos.png";
+import IconoAvionMini from "../assets/icons/IconoAvionBlanco.png";
+import IconoCampanaMini from "../assets/icons/IconoCampanaBlanco.png";
+import IconoTicketMini from "../assets/icons/IconoTicketBlanco.png";
+import LogoPeople from "../assets/icons/LogoPeople.png";
+import IconoAvion from "../assets/icons/IconoAvion.png";
 
 // --- Sub-componentes para mayor claridad ---
 
@@ -17,15 +22,52 @@ const WelcomeLogos = () => (
 );
 
 const WelcomeCallToAction = () => (
-  <div className="flex flex-col justify-center text-center text-lg md:text-4xl text-white gap-5 md:max-w-1/4 md:text-start">
-    <div className="md:leading-12">
-      Estás por vivir la emoción de <br className="sm:hidden" />
-      ganar entradas de la gran final <br className="sm:hidden" />
-      de <strong>Copa Libertadores</strong> <br className="sm:hidden" />
-      o la <strong>Copa Sudamericana</strong>
+  <div className="flex flex-col justify-center text-center text-lg md:text-3xl text-white gap-5 md:max-w-1/4 md:text-start">
+    <div className="md:leading-12 flex relative justify-center w-full">
+      <p>
+        Estás por vivir la emoción de <br className="sm:hidden" />
+        ganar entradas de la gran final <br className="sm:hidden" />
+        de <strong>Copa Libertadores</strong> <br className="sm:hidden" />o la{" "}
+        <strong>Copa Sudamericana</strong>
+      </p>
+      <img
+        src={IconoAvion}
+        alt="Icono de avión"
+        className="w-1/4 md:w-1/4 absolute -top-10 right-0 md:-right-10"
+      />
     </div>
     <div>
-      <BtnLayout text="Ingresar" to="/formulario" />
+      <BtnLayout text="Participa aquí" to="/formulario" />
+    </div>
+    <div className="text-sm md:text-lg" >
+      <div className="flex gap-5">
+        <img className="max-w-3 max-h-3 md:max-w-5 md:max-h-5" src={LogoPeople} alt="LogoPeople" />
+        <div>Incluye el premio para 2 personas:</div>
+      </div>
+      <div className="flex gap-5">
+        <img
+          className="max-w-3 max-h-3 md:max-w-5 md:max-h-5"
+          src={IconoAvionMini}
+          alt="IconoAvionMini"
+        />
+        <div>Vuelos</div>
+      </div>
+      <div className="flex gap-5">
+        <img
+          className="max-w-3 max-h-3 md:max-w-5 md:max-h-5"
+          src={IconoCampanaMini}
+          alt="IconoCampanaMini"
+        />
+        <div>Hospedaje</div>
+      </div>
+      <div className="flex gap-5">
+        <img
+          className="max-w-3 max-h-3 md:max-w-5 md:max-h-5"
+          src={IconoTicketMini}
+          alt="IconoTicketMini"
+        />
+        <div>Entradas al las finales.</div>
+      </div>
     </div>
   </div>
 );
